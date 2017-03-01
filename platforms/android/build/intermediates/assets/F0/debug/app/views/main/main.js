a882ff3069f79e04e77a3f961f83844034913232
Sandesh <073BCT539.sandesh@pcampus.edu.np>
2017-03-01 12:34:13 +0545

    Forced the screen orientationentation

--- platforms/android/build/intermediates/assets/F0/debug/app/views/main/main.js
+++ platforms/android/build/intermediates/assets/F0/debug/app/views/main/main.js
@@ -1,5 +1,9 @@
 var frame = require("ui/frame");
-
+var profiler;
+exports.loaded = function(args){
+    page = args.object;
+    profiler = page.getViewById("profileExpander");
+}
 exports.settings = function(){
     var top = frame.topmost();
     top.navigate({
@@ -12,5 +16,17 @@ exports.settings = function(){
 }
 
 exports.expandProfile = function(args){
-    
+    console.log("Expanding the profile...");
+    profiler.animate({
+        opacity: 0.8,
+        duration: 100
+    })
+}
+exports.subTab = function(args){
+    var img = args.object;
+    img.animate({
+        scalex: 0.5,
+        scaley: 0.5,
+        duration: 100
+    })
 }
